8a7f109dc02a83cb8f4aa317375f08600b6999fe
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-05-02 23:05:37 +0530

    Time: 366 ms (27.22%), Space: 66.4 MB (44.90%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
-    vector<int> indices;                                                 //Stores indices of same group.
-    vector<bool> visited;
-    vector<vector<int>> adjList;
-    string indiceString;                                                 //Stores  string formed by indices in the same group.
-    void dfs(string &s,int n)                                             //DFS to get all indices in same group.
-    {
-        visited[n]=true;
-        indices.push_back(n);
-        indiceString+=s[n];
-        for(int &i:adjList[n])
-            if(!visited[i])
-               dfs(s,i);
-    }
-    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) 
-    {
-        adjList.resize(s.length());
-        visited.resize(s.length(),false);
-        for(vector<int> &v:pairs)                               //Create adjacency list using the indice pairs
-            adjList[v[0]].push_back(v[1]),adjList[v[1]].push_back(v[0]);
-        for(int i=0;i<s.length();i++)
-            if(!visited[i])
-            {
-                indiceString="";                              //Clear string formed by one group of indices before finding next group.
-                indices.clear();                             //Clear indices vector before finding another group.
-                dfs(s,i);
-                sort(indiceString.begin(),indiceString.end());                    //Sort the characters in the same group.
-                sort(indices.begin(),indices.end());                                  //Sort the indices in the same group.            
-                for(int i=0;i<indices.size();i++)          //Replace all the indices in the same group with the sorted characters.
-                    s[indices[i]]=indiceString[i];
+    vector<vector<int>>adjList;
+    vector<bool>vis;
+    vector<int>indices;
+    string indiceString;
+    
+    
+    void dfs(string &s, int i){
+        vis[i] = true;
+        indices.push_back(i);
+        indiceString+=s[i];
+        for(auto &it:adjList[i]){
+            if(!vis[it]){
+                dfs(s,it);
             }
+        }
+    }
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        adjList.resize(s.size());
+        vis.resize(s.size(),false);
+        
+        for(auto &it:pairs){
+            adjList[it[0]].push_back(it[1]);
+            adjList[it[1]].push_back(it[0]);
+        }
+        
+        for(int i = 0 ; i < s.size() ; i++){
+            if(!vis[i]){
+                indices.clear();
+                indiceString = "";
+                dfs(s,i);
+                sort(indices.begin(),indices.end());
+                sort(indiceString.begin(),indiceString.end());
+                for(int i = 0 ; i < indiceString.size() ; i++){
+                    s[indices[i]] = indiceString[i];
+                }
+            }
+        }
         return s;
     }
 };

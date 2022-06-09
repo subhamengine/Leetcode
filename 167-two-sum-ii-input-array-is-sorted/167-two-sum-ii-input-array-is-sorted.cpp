749b9855cacc351a8e7ffcd2fbf7f20c47a5b4d4
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-06-09 09:11:05 +0530

    Time: 13 ms (83.47%), Space: 15.6 MB (75.27%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
-    int search(int target, vector<int>&nums, int i){
-        int j = nums.size()-1;
-        while(i<=j){
-            int mid = (i+j)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] < target){
-                i = mid+1;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i = 0, j = nums.size()-1;
+        while(i < j){
+            if(nums[i] + nums[j] == target) return {i+1,j+1};
+            else if(nums[i] + nums[j] < target){
+                i++;
             }
-            else j = mid-1;
-        }
-        return -1;
-    }
-    vector<int> twoSum(vector<int>& nums, int tar) {
-        int n = nums.size();
-        for(int i = 0 ; i < nums.size() ; i++){
-            int num = nums[i];
-            int nextNumIndex = search(tar-num,nums,i+1);
-            if(nextNumIndex != -1){
-                return {i+1,nextNumIndex+1};
-            }
+            else j--;
         }
         return {};
     }

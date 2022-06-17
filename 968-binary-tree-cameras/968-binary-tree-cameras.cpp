af47b1670fd837441a37107cb37c22f629f3d404
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-06-17 22:21:56 +0530

    Time: 16 ms (57.04%), Space: 21.3 MB (55.06%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    #define NO_CAMERA       0
+    #define HAS_CAMERA      2
+    #define NOT_NEEDED      1
+    int ans = 0;
+    int dfs(TreeNode *root) {
+        if (!root) return NOT_NEEDED;
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        if (l == NO_CAMERA || r == NO_CAMERA) {
+            ans++;
+            return HAS_CAMERA;
+        } else if (l == HAS_CAMERA || r == HAS_CAMERA) {
+            return NOT_NEEDED;
+        } else {
+            return NO_CAMERA;
+        }
+    }
+    int minCameraCover(TreeNode* root) {
+        if (dfs(root) == NO_CAMERA) ans++;
+        return ans;
+    }
+};

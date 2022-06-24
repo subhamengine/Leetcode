fc835e7798afd401bca482de650a832187aacf6f
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-06-24 12:31:14 +0530

    Time: 2 ms (56.84%), Space: 7.7 MB (46.44%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -11,13 +11,16 @@ public:
 //     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n,-1);
+        
         if(n==1)return nums[0];
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int ptr1 = nums[0];
+        int ptr2 = max(nums[0],nums[1]);
+        
         for(int i = 2 ; i < n ; i++){
-            dp[i] = max(nums[i] + dp[i-2] , dp[i-1]);
+            int newNum = max(nums[i] + ptr1 , ptr2);
+            ptr1 = ptr2;
+            ptr2 = newNum;
         }
-        return dp[n-1];
+        return ptr2;
     }
 };

5b09b58e3d67a32a85497bf0813d49cdb39225f2
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-08-09 15:55:05 +0530

    Time: 34 ms (47.85%), Space: 12.7 MB (63.64%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size(), m = str2.size();
-        
-        vector<vector<int>>dp(n+1,vector<int>(m+1));
-        for(int i = 0 ; i < n+1 ; i++){
-            for(int j = 0 ; j < m+1 ; j++){
-                if(i == 0 or j == 0)dp[i][j] = 0;
+    string shortestCommonSupersequence(string a, string b) {
+        int n = a.size(), m = b.size();
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = 1 ; j <= m ; j++){
+            if(a[i-1] == b[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
             }
+            else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
         }
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < m+1 ; j++){
-                if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
+    }
+    int i = n , j = m ;
+    string ans = "";
+    while(i > 0  and j > 0){
+        if(a[i-1] == b[j-1]){
+            ans+=a[i-1];
+            i--,j--;
         }
-        int l = n , r = m;
-        string toReturn;
-        while(l > 0 and r > 0){
-            if(str1[l-1] == str2[r-1]){
-                toReturn.push_back(str1[l-1]);
-                l = l-1, r = r-1;
+        else{
+            if(dp[i-1][j] > dp[i][j-1]){
+                ans+=a[i-1];
+                i--;
             }
             else{
-                dp[l-1][r] > dp[l][r-1]?l = l-1:r = r-1;
+                ans+=b[j-1];
+                j--;
             }
         }
-        reverse(toReturn.begin(),toReturn.end());
-        int i = 0, j = 0;
-        string res = "";
-        for (char c : toReturn) {
-            while (str1[i] != c)
-                res += str1[i++];
-            while (str2[j] != c)
-                res += str2[j++];
-            res += c, i++, j++;
-        }
-        return res + str1.substr(i) + str2.substr(j);
+     }
+    while(j > 0){
+        ans+=b[j-1];
+        j--;
+    }
+    while(i > 0){
+        ans+=a[i-1];
+        i--;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
         
     }
 };

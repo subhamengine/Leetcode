8bb160f6ba189beb6018092ec582f10a888409c8
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-08-16 20:37:35 +0530

    Time: 48 ms (58.37%), Space: 13.1 MB (93.53%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    int rankCount(vector<vector<int>>& matrix, int target){
-        int n = matrix.size();
-        int i = n-1 , j = 0, count = 0;
-        while(i>=0 and j < n){
-            if(matrix[i][j] > target){
-                i--;
-            }
-            else{
-                count = count + i + 1;
-                j++;
-            }
-        }
-        return count;
-    }
+    
     
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size(), l = matrix[0][0], h = matrix[n-1][n-1];
         while(l<h){
             int mid = l + (h - l)/2;
-            int count = rankCount(matrix,mid);
+            int count = 0;
+            for(int i = 0 ; i < n ; i++){
+                count+= upper_bound(matrix[i].begin() , matrix[i].end() , mid) - matrix[i].begin();
+            }
+            cout<<mid <<" -> "<<count<<endl;
             if(count < k) l = mid+1;
             else h = mid;
         }

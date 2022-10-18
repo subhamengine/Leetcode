4e14c6505463fb9732a9f7998c7be50ccba80fe3
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-10-18 21:38:49 +0530

    Time: 18 ms (47.09%), Space: 7.2 MB (26.72%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string countAndSay(int n, string s = "1") {
+    if(n==1) return s;
+    int i=0, j, len = s.size();
+    string ans = "";
+    while(i<len) {
+        j=i;
+        while(i<len && s[i]==s[j]) i++; // substring is [j,i-1] & "i-j" is the count
+        ans += to_string(i-j) + s[j];
+    }
+    return countAndSay(n-1, ans);
+}
+};

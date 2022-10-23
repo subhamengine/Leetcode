fe5158df4cd45d5b9d06d7c529a858a2cca47943
Subham Sharma <83713146+subhamengine@users.noreply.github.com>
2022-10-23 19:52:31 +0530

    Time: 90 ms (39.98%), Space: 21.4 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        map<int,int>mp;
-        int num = 0;
-        for(auto it:nums){
-            mp[it]++;
-            if(mp[it] == 2){
-                num = it;
-               
-            }
+        for(int i = 0; i<nums.size(); i++){
+            while(nums[i] != nums[nums[i] - 1])swap(nums[i], nums[nums[i] - 1]);
         }
-        vector<int>ans;
-        ans.push_back(num);
-        int i = 1;
-        for( i = 1; i <= *max_element(nums.begin(),nums.end()) ;i++){
-            if(mp.find(i) == mp.end()){
-                ans.push_back(i);
-                break;
-            }
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] != i+1) return {nums[i],i+1};
         }
-        if(ans.size() == 1){
-            ans.push_back(i);
-        }
-        return ans;
+        return {};
     }
 };
